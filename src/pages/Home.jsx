2bfa00a17d823ebea68cc05b1d23300ad3f123ca
Chisom <83100384+titanwarlord007@users.noreply.github.com>
2023-03-16 23:30:57 +0100

    home page animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,25 +20,21 @@ function Home() {
       className=" container home capitalize text-center center "
       onClick={isOpen ? close : undefined}
     >
-      <div className="zigzag"></div>
       <div className="content">
         <h1 className="typed typed-1">chisom okereke</h1>
-        <h3 className="typed typed-2">frontend developer</h3>
+        <h3 className="fade-in">frontend developer</h3>
         <br />
         <div className="icon-container fade-in flex-wrap">
-          {
-            skills.map((skill, index) => {
+          {skills.map((skill, index) => {
             const { text, icon } = skill;
-              
-              return (
-                <div className="flex-col">
-                  {icon}
-                  <p>{text}</p>
-                </div>
-              );
-            })
-          }
 
+            return (
+              <div className="flex-col">
+                {icon}
+                <p>{text}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
